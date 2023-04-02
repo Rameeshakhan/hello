f3c6b36cd12aa958320fbac974d06d00c2e8d174
Rameesha Khan <rameeshakhan75@gmail.com>
2023-04-03 01:09:19 +0500

    Revert "Rameesha Second Commit"
    This reverts commit 51893755d6268e6aae43212a16ea662fb529f3a2.

--- Index.js
+++ Index.js
@@ -2,5 +2,4 @@ console.log("HI this is Rameesha")
 console.log("This is second line")
 console.log("This is third line")
 console.log("This change is added by rameesha")
-console.log("This is an another change  added by rameesha")
 
